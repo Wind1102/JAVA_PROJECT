1b6ece20d10f6f1f2f1f2612a13620386c8d625d
Wind1102 <Hieu.vm02@gmail.com>
2025-01-13 12:02:48 +0700

    update

--- identity-service/src/main/java/com/minhhieu/identity_service/exception/GlobalExceptionHandler.java
+++ identity-service/src/main/java/com/minhhieu/identity_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.minhhieu.identity_service.exception;
 
 import com.minhhieu.identity_service.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @ControllerAdvice()
 public class GlobalExceptionHandler {
 
@@ -47,15 +50,21 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(ex.getBindingResult().getFieldErrors().stream().map(error -> {
             String enumKey = error.getDefaultMessage();
             ErrorCode errorCode = ErrorCode.INVALID_KEY;
+            ApiResponse apiResponse = new ApiResponse();
             try {
                 errorCode = ErrorCode.valueOf(enumKey);
+                var constraintViolations = ex.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+                var attrs = constraintViolations.getConstraintDescriptor().getAttributes();
+                var min_age = attrs.get("min");
+                var message = String.format(errorCode.getMessage(),min_age.toString());
+                log.info(min_age.toString());
+                log.info(message);
+                apiResponse.setCode(errorCode.getCode());
+                apiResponse.setMessage(message);
             } catch (IllegalArgumentException e) {
 
             }
 
-            ApiResponse apiResponse = new ApiResponse();
-            apiResponse.setCode(errorCode.getCode());
-            apiResponse.setMessage(errorCode.getMessage());
             return apiResponse;
         }).collect(Collectors.toList()));
     }
